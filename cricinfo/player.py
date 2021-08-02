ade838f88719be1e107eebbb6f04cb48337b2483
James <jmw1982@gmail.com>
2021-08-02 15:28:22 +1000

    missing file from previous commit

--- cricinfo/player.py
+++ cricinfo/player.py
@@ -1,9 +1,13 @@
 import asyncio
 import re
 
+from collections import defaultdict
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 from .cricinfo import CricinfoGeneric, _bulk_coro_wrapper
+from .player_career_stats import PlayerCareerStats
+from .match_list_stats import MatchListStats
 
 class Player(CricinfoGeneric):
 
@@ -23,7 +27,7 @@ class Player(CricinfoGeneric):
 		'player_matches': {
 			# class=1 - Test, class=2 - ODI, class=3 - T20I,
 			# class=11 - Test/ODI/T20I, class=20 - Youth Test, class=21 - Youth ODI, 
-			# ../player/[num].html/?class={1,2,3}&template=results&type=allround&view=match
+			# ../player/[num].html/?class={1,2,3,11,20,20,21};template=results;type=allround;view=match
 			'url_base': 'https://stats.espncricinfo.com/ci/engine/player/',
 			'params': [
 				'class',
@@ -53,6 +57,8 @@ class Player(CricinfoGeneric):
 		self.bowling_style = None
 		self.born = None
 		self.teams = []
+		self.career_stats_dict = {}
+		self.match_list_stats_dict = defaultdict(list)
 
 		super()._set_attrs_from_kwarg_d(kwargs)
 
@@ -70,7 +76,7 @@ class Player(CricinfoGeneric):
 
 		ret_players = []
 
-		(search_resp, search_resp_text,) = await Player._coro_req(endpoint_k='player_search',
+		(_, search_resp_text,) = await Player._coro_req(endpoint_k='player_search',
 			param_d={'search': search_str, 'type': 'player'}
 		)
 
@@ -204,5 +210,186 @@ class Player(CricinfoGeneric):
 	def get_details(self):
 		asyncio.run(self.coro_get_details())
 
-	async def coro_get_match_summaries(self):
-		pass
+	async def coro_get_match_summaries_career_stats(self, match_selector='senior-international'):
+
+		match_selector_d = {
+			'senior-international': 11,
+			'test': 1,
+			'odi': 2,
+			't20i': 3,
+			'youth-test': 20,
+			'youth-odi': 21
+		}
+		match_selector_num = match_selector_d.get(match_selector)
+		if not match_selector_num:
+			raise ValueError(f"Unknown match selector: {match_selector}")
+
+		url_suffix = f"{self.id}.html"
+
+		(_, http_resp_text) = await Player._coro_req(
+			'player_matches', param_d={
+				'class': match_selector_num,
+				'template': 'results',
+				'type': 'allround',
+				'view': 'match'
+			},
+			url_suffix=url_suffix
+		)
+
+		bs = BeautifulSoup(http_resp_text, 'html.parser')
+
+		# <caption>Career averages</caption>
+		# the <table> that is the parent of this is what we want
+		captions = bs.find_all('caption')
+		if not captions or len(captions) < 2:
+			raise ValueError(f"Missing <caption> tags")
+
+		if captions[0].string == 'Career averages':
+			averages_table = captions[0].parent
+
+		if averages_table.name != 'table':
+			raise ValueError(f"parent tag of averages caption is not <table>")
+
+		averages_thead = averages_table.find('thead')
+		averages_thead_tr = averages_thead.find('tr')
+
+		# check order of string values in <th>s in <thead><tr>
+		# as this determines the order of stats <td>s in <tbody><tr>
+		field_indices = []
+		for averages_th in averages_thead_tr.find_all('th'):
+			# otherwise we get 'None' instead of None and that's annoying
+			field_name = str(averages_th.string) if averages_th.string else None
+			field_indices.append(field_name)
+
+		averages_tbody = averages_table.find('tbody')
+		averages_tbody_tr = averages_tbody.find('tr')
+
+		stats_obj = PlayerCareerStats()
+		for (i, averages_td,) in enumerate(averages_tbody_tr.find_all('td')):
+			
+			cur_val = str(averages_td.string)
+
+			# if this belongs to a blank header, skip it
+			if not field_indices[i]:
+				continue
+
+			elif field_indices[i] == 'Span':
+				stats_obj.career_year_span = cur_val
+
+			elif field_indices[i] == 'Mat':
+				stats_obj.matches = int(cur_val) 
+
+			elif field_indices[i] == 'Runs':
+				stats_obj.runs = int(cur_val)
+
+			elif field_indices[i] == 'HS':
+				stats_obj.highest_score = int(cur_val)
+
+			elif field_indices[i] == 'Bat Av':
+				stats_obj.batting_average = float(cur_val)	
+
+			elif field_indices[i] == '100':
+				stats_obj.hundreds = int(cur_val)
+
+			elif field_indices[i] == 'Wkts':
+				stats_obj.wickets = int(cur_val)
+
+			elif field_indices[i] == 'BBI':
+				stats_obj.best_bowling_innings = cur_val
+
+			elif field_indices[i] == 'Bowl Av':
+				stats_obj.bowling_average = float(cur_val)
+
+			elif field_indices[i] == '5':
+				stats_obj.five_wickets_innings = int(cur_val)
+
+			elif field_indices[i] == 'Ct':
+				stats_obj.catches = int(cur_val)
+
+			elif field_indices[i] == 'St':
+				stats_obj.stumpings = int(cur_val)
+
+			# we don't need to store this specifically
+			elif field_indices[i] == 'Ave Diff':
+				continue
+
+			else:
+				raise ValueError(f"Unknown field name {field_indices[i]}")
+
+			self.career_stats_dict[match_selector] = stats_obj
+
+		# <caption>Match by match list</caption>
+		# similar to above but create MatchListStats instances in list at
+		# self.match_list_stats_dict[match_selector]
+		if captions[1].string == 'Match by match list':
+			match_list_table = captions[1].parent
+
+		match_list_thead = match_list_table.find('thead')
+		match_list_thead_tr = match_list_thead.find('tr')
+
+		field_indices = []
+		for match_list_th in match_list_thead_tr.find_all('th'):
+			# otherwise we get 'None' instead of None and that's annoying
+			field_name = str(match_list_th.string) if match_list_th.string else None
+			field_indices.append(field_name)
+
+		match_list_tbody = match_list_table.find('tbody')
+		match_list_tbody_tr = match_list_tbody.find('tr')
+
+		match_stats_obj = MatchListStats()
+		for (i, match_td,) in enumerate(match_list_tbody_tr.find_all('td')):
+
+			cur_val = str(match_td.string)
+
+			if field_indices[i] == 'Bat1':
+				(num_score, not_out,) = MatchListStats.parse_score(cur_val)
+				match_stats_obj.first_innings_score = num_score
+				match_stats_obj.first_innings_not_out = not_out
+
+			elif field_indices[i] == 'Bat2':
+				(num_score, not_out,) = MatchListStats.parse_score(cur_val)
+				match_stats_obj.second_innings_score = num_score
+				match_stats_obj.second_innings_not_out = not_out
+
+			# ignore total runs as it's trivial
+			elif field_indices[i] == 'Runs':
+				continue
+
+			elif field_indices[i] == 'Wkts':
+				match_stats_obj.total_wickets = int(cur_val)
+
+			elif field_indices[i] == 'Conc':
+				match_stats_obj.runs_conceded = int(cur_val)
+
+			elif field_indices[i] == 'Ct':
+				match_stats_obj.catches = int(cur_val)
+
+			elif field_indices[i] == 'St':
+				match_stats_obj.stumpings = int(cur_val)
+
+			elif field_indices[i] == 'Opposition':
+				match_stats_obj.opposition = cur_val
+
+			elif field_indices[i] == 'Ground':
+				match_stats_obj.ground = cur_val
+
+			elif field_indices[i] == 'Start Date':
+				# '2 Dec 1999'
+				match_stats_obj.start_dt = datetime.strptime(cur_val, "%d %b %Y")
+
+			# blank header, specifically at the end, is where the link to the scorecard is
+			# there are a couple of others, ignore them
+			elif not field_indices[i]:
+				if i == len(field_indices) - 1:
+					scorecard_a_tag = match_td.find('a')
+					match_stats_obj.scorecard_url_path = str(scorecard_a_tag['href'])
+
+					#TODO - parse match ID out of this and store it in match_stats_obj
+
+			else:
+				raise ValueError(f"Unknown match list column header: {field_indices[i]}")
+
+		self.match_list_stats_dict[match_selector].append(match_stats_obj) 
+
+	def get_match_summaries_career_stats(self, match_selector='senior-international'):
+		asyncio.run(self.coro_get_match_summaries_career_stats(match_selector))
