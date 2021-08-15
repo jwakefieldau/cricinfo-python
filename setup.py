fef27794def1dbe2261785d7a298923c59040a3f
James <jmw1982@gmail.com>
2021-08-15 21:02:07 +1000

    fix deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name='cricinfo-python',
 	version='0.1',
 	description='Asynchronous scraper for cricinfo.',
-	requires=[
+	install_requires=[
 		'aiohttp (>=3.7.4)',
 		'aiodns (>=3.0.0)',
 		'beautifulsoup4 (>=4.9.3)',
